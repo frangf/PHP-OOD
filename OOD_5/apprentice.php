ffbfc3ff574250a486fa6824cac6e34a57b357ae
Francisco <72940617+frangf@users.noreply.github.com>
2021-01-31 20:41:55 +0100

    Add files via upload
    Ejercicio OOD 5

---
+++ OOD_5/apprentice.php
@@ -0,0 +1,28 @@
+<?php
+class Apprentice extends Student
+{
+private $FCTBusinessName;
+
+public function setFCTBusinessName($FCTBusinessName)
+{
+$this->FCTBusinessName=$FCTBusinessName;
+}
+public function get_FCTBusinessName() {
+  return $this->FCTBusinessName;
+}
+/*
+public function __construct($name,$dni,$FCTBusinessName)
+{
+  parent::__construct($name,$dni);
+  $this->FCTBusinessName=$FCTBusinessName;
+}
+*/
+public function print()
+{
+parent::print();
+echo "<b>FCTBusinessName:</b> $this->FCTBusinessName.<br>";
+}
+
+
+}
+?>
---
+++ OOD_5/student.php
@@ -0,0 +1,21 @@
+<?php
+class Student extends Person
+{
+private $studyField;
+
+public function setStudyField($studyField)
+{
+  $this->studyField=$studyField;
+}
+
+public function getstudyField() {
+  return $studyField->studyField;
+}
+
+public function print()//overriding print method from parent class Person
+{
+parent::print();//calling print method from parent class Person
+echo "<b>StudyField:</b> $this->studyField.<br>";
+}
+}
+?>
