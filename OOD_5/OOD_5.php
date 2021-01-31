9765037fe8218a5bd2b68031adcfb7e04d1bc66f
fran <franexyt@gmail.com>
2021-01-31 20:40:45 +0100

    Ejercicio OOD 5

---
+++ OOD_5/OOD_5.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>OOD_5: inheritance example</title>
+</head>
+<body>
+<?php
+require "person.php";//similar to include
+require "student.php";
+require "teacher.php";
+require "Apprentice.php";
+
+$teacher1=new Teacher('Franki','12345678A',22);//using __construct from Person
+$teacher1->setSalary(2200);
+$student1=new Apprentice('Francisco','98745612R');
+$student1->setStudyField('ASIX');
+$student1->setFCTBusinessName('IES Manacor');
+
+echo $teacher1->print();//using method print implemented on Person class
+echo $student1->print();//using method print implemented on Student class
+
+ ?>
+</body>
+</html>
---
+++ OOD_5/person.php
@@ -0,0 +1,38 @@
+<?php
+class Person
+{
+public $name;
+public $dni;
+
+public function setname($name)
+{
+  $this->name=$name;
+}
+
+function get_name() {
+  return $this->name;
+}
+
+public function setDni($dni)
+{
+  $this->dni=$dni;
+}
+
+function get_dni() {
+  return $this->dni;
+}
+
+public function __construct($name,$dni)
+{
+$this->name=$name;
+$this->dni=$dni;
+}
+//no podem posar protected ja que ood_5 necessita aquest print
+public function print()
+{
+  echo "<b>Name:</b> $this->name.<br>";
+  echo "<b>DNI:</b> $this->dni<br>";
+}
+
+}
+?>
